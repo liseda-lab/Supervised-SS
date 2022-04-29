d8a136c8479758d66f10399a265fb04f7d80b76e
ritatsousa <53748834+ritatsousa@users.noreply.github.com>
2022-04-29 14:09:03 +0100

    Update run_OpenKEmodel.py

--- SS_Calculation/run_OpenKEmodel.py
+++ SS_Calculation/run_OpenKEmodel.py
@@ -257,6 +257,7 @@ def run_embedddings_aspect(ontology, ontology_annotations, ontology_annotations_
     ontology_kg = rdflib.Graph()
     ontology_kg.parse(ontology, format='xml')
 
+    list_embeddings_files = []
     for i in range(len(aspects)):
         aspect = aspects[i]
         name_aspect = name_aspects[i]
@@ -267,13 +268,16 @@ def run_embedddings_aspect(ontology, ontology_annotations, ontology_annotations_
 
         path_output = 'SS_Calculation/OpenKE/'
         path_model_json = path_output + model_embedding + "/embedding_" + aspect + ".vec.json"
-        ensure_dir(dataset_output)
         ensure_dir(path_output + model_embedding)
-        path_embeddings_output = path_embedding + '/' + name_aspect + '/'  + 'Embeddings_' + '_' + model_embedding + "_" + name_aspect + '.txt'
 
+        path_embeddings_output = path_embedding + '/' + name_aspect + '/'  + 'Embeddings_' + '_' + model_embedding + "_" + name_aspect + '.txt'
         dic_nodes, dic_relations, list_triples = process_KG.buildIds(Graph)
         construct_model(dic_nodes, dic_relations, list_triples, path_output, model_embedding, name_aspect)
         write_embeddings(path_model_json, path_embeddings_output, ents, dic_nodes)
+        list_embeddings_files.append(path_embeddings_output)
+
+    return list_embeddings_files
+
 
 
 
